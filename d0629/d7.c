4cd763d864a1a6a25bfe216dd08469bddecf0e4d
chaem <zhdkffk1220@naver.com>
2016-06-29 13:48:10 +0900

    ...

---
+++ d0629/d7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d \r\n",10%3); // 나머지값
+	return 0;
+}
---
+++ d0629/day0629.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *art = "&#9734";
+	printf("%c",art);
+	return 0;
+}
