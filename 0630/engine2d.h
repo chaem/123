6c241ba43a44db6da5547a7bf1d645e393786a56
chaem <zhdkffk1220@naver.com>
2016-06-30 17:13:02 +0900

    ...

--- 0630/engine2d.h
+++ 0630/engine2d.h
@@ -1,14 +1,33 @@
 #ifndef __ENGIINE_2D__
 #define __ENGIINE_2D__
 
+// setting color
 void setColor(int fore_color,int back_color)
 {
 	printf("%c[%d;%dm",0x1b,fore_color,back_color);
 }
 
+// location  y=line
 void gotoxy(int x,int y)
 {
 	printf("%c[%d;%df",0x1b,y,x);
 }
 
+// create box
+int drawMyBox(int nXpos,int nYpos,int nColor,int nSize)
+{
+	int ix,iy;
+
+	setColor(30,nColor);
+	ix = 0;
+	for(iy = 0; iy <= nSize; iy++) {
+		for(ix = 0; ix <= nSize; ix++) {
+			gotoxy(ix+nXpos,iy+nYpos);
+			printf(" ");
+
+		}
+		setColor(0,0);
+
+	}
+}
 #endif
--- 0630/ex_engine3.c
+++ 0630/ex_engine3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "engine2d.h"
-
+/*
 int main()
 {
 	system("clear"); //window clean
@@ -24,5 +24,43 @@ int main()
 	gotoxy(0,30);
 	printf("==============="); //end
 
+	return 0;
+}
+*/
+
+// error
+void drawMyBox(int as,int df,int color) 
+{
+	int x,y;
+	x = 1;
+	y = 1;
+
+	setColor(30,color);
+ 	while(x <= 4) {
+		y = 1;
+		while(y <= 2) {
+			gotoxy(x+as,y+df);
+			printf(" ");
+			y++;
+		
+		}
+		x++;
+	}
+	setColor(0,0);
+
+}
+
+int main()
+{
+	system("clear");
+	
+	drawMyBox(1,10); // draw all
+	drawMyBox(10,20);
+
+	setColor(30,44);
+	gotoxy(0,45);
+	printf("           ");
+	setColor(0,0);
+	
 	return 0;
 }
